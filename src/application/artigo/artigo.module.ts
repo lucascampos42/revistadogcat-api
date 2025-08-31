080c1024f1cc9e553f419246325a0d1f1ce81a93
lucascampos42 <bhlucascampos@gmail.com>
2025-08-30 23:05:10 -0300

    fix: artigos

--- src/application/artigo/artigo.module.ts
+++ src/application/artigo/artigo.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ArtigoService } from './artigo.service';
 import { ArtigoController } from './artigo.controller';
 import { ArtigoRepository } from './repositories/artigo.repository';
+import { ComentarioRepository } from './repositories/comentario.repository'; // Import the new repository
 import { PrismaModule } from '../../core/config/prisma.module';
 import { FileUploadModule } from '../../core/services/file-upload.module';
 
 @Module({
   imports: [PrismaModule, FileUploadModule],
   controllers: [ArtigoController],
-  providers: [ArtigoService, ArtigoRepository],
-  exports: [ArtigoService, ArtigoRepository],
+  providers: [ArtigoService, ArtigoRepository, ComentarioRepository], // Add it to the providers array
+  exports: [ArtigoService, ArtigoRepository, ComentarioRepository], // Also export it
 })
 export class ArtigoModule {}
